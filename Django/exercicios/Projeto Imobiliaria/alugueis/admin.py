931efa6f89994878f00c6bde29fbefa03747e047
Sarah Michelly <marcio320320@gmail.com>
2024-11-04 10:05:46 -0300

    deletando

--- Django/exercicios/Projeto Imobiliaria/alugueis/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import ImovelAluguel
-from .models import Contrato
-
-admin.site.register(ImovelAluguel)
-admin.site.register(Contrato)
--- Django/exercicios/Projeto Imobiliaria/contato/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Contato(models.Model):
-    nome = models.CharField(max_length=150)
-    email = models.EmailField()
-    mensagem = models.TextField()
-
-    def __str__(self):
-        return self.nome
